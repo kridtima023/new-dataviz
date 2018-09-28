10dc643f287c6566cf4a85a26140e0ea7809a88a
5830213036 <s5830213036@phuket.psu.ac.th>
2018-09-28 16:05:18 +0700

    Update viz

--- src/router/index.js
+++ src/router/index.js
@@ -4,12 +4,9 @@ import Navbar from '@/components/User/Navbar'
 import Addblog from '@/components/Admin/Addblog'
 import Signin from '@/components/Admin/Signin'
 import Detail from '@/components/User/Detail'
-<<<<<<< HEAD
 import Editblog from '@/components/Admin/Editblog'
 import Detailblog from '@/components/Admin/Detailblog'
-=======
 import Readmore from '@/components/User/Readmore'
->>>>>>> ba72ffa2148c549bcd09d03b7b1c9f0398b91a6a
 
 Vue.use(Router)
 
@@ -38,7 +35,6 @@ export default new Router({
     }
     ,
     {
-<<<<<<< HEAD
       path: '/edit-blog/:edit_slug',
       name: 'Editblog',
       component: Editblog
@@ -51,11 +47,7 @@ export default new Router({
     }
 
 
-=======
-      path: '/readmore',
-      name: 'Readmore',
-      component: Readmore
-    }
->>>>>>> ba72ffa2148c549bcd09d03b7b1c9f0398b91a6a
+  
+    
   ]
 })
